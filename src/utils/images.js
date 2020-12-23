a20980f609b7a20159ede5b9a0078888e7022a5b
christian <rollawaypoint@gmail.com>
2020-12-23 22:21:01 +0800

    Test

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,37 +2,24 @@ const fs = require('fs')
 
 module.exports.read_photos = () => {
   try {
-    return fs.readdirSync('../7419-images/photos')
+    // return fs.readdirSync('../7419-images/photos')
+    return fs.readdirSync('/root/christian/7419-images/photos')
   } catch (e) {
     return []
   }
 }
 
-module.exports.write_photos = ({ base64, name }) => {
+module.exports.write_photos = async ({ base64, name }) => {
   try {
-    const path = `../7419-images/photos/${name}.png`
-    const dataBuffer = Buffer(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+    const path = `./${name}.jpg`
+    const buffer = Buffer.from(base64.replace(/^ *data:image\/\w+;base64,/, ''), 'base64')
 
-    if (Buffer.isBuffer(dataBuffer)) {
-      fs.writeFile(path, dataBuffer, function(err) {
-        //用fs写入文件
-        if (err) {
-          return {
-            code: 500,
-            msg: 'Write file failed.',
-          }
-        }
+    fs.writeFileSync(path, buffer)
 
-        return {
-          code: 200,
-          msg: '',
-        }
-      })
+    return {
+      msg: 'success',
     }
   } catch (e) {
-    return {
-      code: 500,
-      msg: e,
-    }
+    return `try!, ${JSON.stringify(e)} ${name}`
   }
 }
