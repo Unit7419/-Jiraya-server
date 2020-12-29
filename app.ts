9b424f52ff24059437a20fc6cf439f30579ccb01
justwink <rollawaypoint@gmail.com>
2020-12-29 16:25:19 +0800

    Remove useless package

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import * as Koa from 'koa'
 import * as cors from 'koa2-cors'
 
-const bodyParser = require('koa-bodyparser')
 const body = require('koa-body')
 const router = require('./router')
 const middleWare = require('./middleware')
@@ -9,7 +8,6 @@ const middleWare = require('./middleware')
 export const createHttpServer = () => {
   const app = new Koa()
 
-  // app.use(bodyParser())
   app.use(body())
   app.use(cors())
   middleWare(app)
